f619fb6385d972cb15a9f208a44bcd85abdb543a
kalytka <ykalytka@thingsboard.io>
2023-11-13 11:56:22 +0200

    testing

--- src/app/modules/home/components/widget/config/widget-config-components.module.d.ts
+++ src/app/modules/home/components/widget/config/widget-config-components.module.d.ts
@@ -12,12 +12,14 @@ import * as i10 from "@home/components/widget/config/timewindow-style.component"
 import * as i11 from "@home/components/widget/config/timewindow-style-panel.component";
 import * as i12 from "@home/components/widget/config/timewindow-config-panel.component";
 import * as i13 from "@home/components/widget/config/widget-settings.component";
-import * as i14 from "@angular/common";
-import * as i15 from "@app/shared/shared.module";
-import * as i16 from "@home/components/widget/lib/settings/widget-settings.module";
-import * as i17 from "@home/components/widget/lib/settings/common/widget-settings-common.module";
+import * as i14 from "@home/components/widget/config/basic/common/data-keys-panel.component";
+import * as i15 from "@home/components/widget/config/basic/common/data-key-row.component";
+import * as i16 from "@angular/common";
+import * as i17 from "@app/shared/shared.module";
+import * as i18 from "@home/components/widget/lib/settings/widget-settings.module";
+import * as i19 from "@home/components/widget/lib/settings/common/widget-settings-common.module";
 export declare class WidgetConfigComponentsModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<WidgetConfigComponentsModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<WidgetConfigComponentsModule, [typeof i1.AlarmAssigneeSelectComponent, typeof i2.AlarmFilterConfigComponent, typeof i3.DataKeysComponent, typeof i4.DataKeyConfigDialogComponent, typeof i5.DataKeyConfigComponent, typeof i6.DatasourceComponent, typeof i7.DatasourcesComponent, typeof i8.EntityAliasSelectComponent, typeof i9.FilterSelectComponent, typeof i10.TimewindowStyleComponent, typeof i11.TimewindowStylePanelComponent, typeof i12.TimewindowConfigPanelComponent, typeof i13.WidgetSettingsComponent], [typeof i14.CommonModule, typeof i15.SharedModule, typeof i16.WidgetSettingsModule, typeof i17.WidgetSettingsCommonModule], [typeof i1.AlarmAssigneeSelectComponent, typeof i2.AlarmFilterConfigComponent, typeof i3.DataKeysComponent, typeof i4.DataKeyConfigDialogComponent, typeof i5.DataKeyConfigComponent, typeof i6.DatasourceComponent, typeof i7.DatasourcesComponent, typeof i8.EntityAliasSelectComponent, typeof i9.FilterSelectComponent, typeof i10.TimewindowStyleComponent, typeof i11.TimewindowStylePanelComponent, typeof i12.TimewindowConfigPanelComponent, typeof i13.WidgetSettingsComponent, typeof i17.WidgetSettingsCommonModule]>;
+    static ɵmod: i0.ɵɵNgModuleDeclaration<WidgetConfigComponentsModule, [typeof i1.AlarmAssigneeSelectComponent, typeof i2.AlarmFilterConfigComponent, typeof i3.DataKeysComponent, typeof i4.DataKeyConfigDialogComponent, typeof i5.DataKeyConfigComponent, typeof i6.DatasourceComponent, typeof i7.DatasourcesComponent, typeof i8.EntityAliasSelectComponent, typeof i9.FilterSelectComponent, typeof i10.TimewindowStyleComponent, typeof i11.TimewindowStylePanelComponent, typeof i12.TimewindowConfigPanelComponent, typeof i13.WidgetSettingsComponent, typeof i14.DataKeysPanelComponent, typeof i15.DataKeyRowComponent], [typeof i16.CommonModule, typeof i17.SharedModule, typeof i18.WidgetSettingsModule, typeof i19.WidgetSettingsCommonModule], [typeof i1.AlarmAssigneeSelectComponent, typeof i2.AlarmFilterConfigComponent, typeof i3.DataKeysComponent, typeof i4.DataKeyConfigDialogComponent, typeof i5.DataKeyConfigComponent, typeof i6.DatasourceComponent, typeof i7.DatasourcesComponent, typeof i8.EntityAliasSelectComponent, typeof i9.FilterSelectComponent, typeof i10.TimewindowStyleComponent, typeof i11.TimewindowStylePanelComponent, typeof i12.TimewindowConfigPanelComponent, typeof i13.WidgetSettingsComponent, typeof i19.WidgetSettingsCommonModule, typeof i14.DataKeysPanelComponent, typeof i15.DataKeyRowComponent]>;
     static ɵinj: i0.ɵɵInjectorDeclaration<WidgetConfigComponentsModule>;
 }
