c22948e2a2ff1cd97da714829aab352bfb8aee84
kalytka <ykalytka@thingsboard.io>
2023-11-14 11:52:28 +0200

    testing

--- src/app/modules/home/components/public-api.d.ts
+++ src/app/modules/home/components/public-api.d.ts
@@ -1,4 +1,2 @@
 export * from './home-components.module';
 export * from './widget/config/widget-config.component.models';
-export * from './widget/config/datasources.component';
-export * from './widget/config/basic/common/data-keys-panel.component';
